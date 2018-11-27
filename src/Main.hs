4a25af698327f4f0437cd5c425f6bd878273b128
Adrian Marina <adrianmarina.adrian@gmail.com>
2018-11-27 08:24:47 +0000

    Use stm bounded channel do communicate to postmans
    No concurency right now therefore the first postman
    empties the channel leaving nothing to do for the
    rest of the postmans.
    
    Nex,t I'll add forkIO to each postman so that they
    deliver messages concurently.

--- src/Main.hs
+++ src/Main.hs
@@ -1,5 +1,90 @@
+{-# LANGUAGE OverloadedStrings #-}
+
 module Main where
+
+import Data.Text
+import Data.Foldable
+import Control.Monad
+import Control.Concurrent.STM.TBMChan
+import GHC.Conc
+
+data Card
+  = Card
+      { from    :: Text
+      , to      :: Text
+      , subject :: Text
+      , message :: Text
+      }
+  deriving (Show)
+
+type CardChan = TBMChan Card
+
+data Receiver
+  = Receiver
+      { name  :: Text
+      , email :: Text
+      }
+
+receivers :: [Receiver]
+receivers
+  = [ Receiver { name = "Adrian", email = "adrian@email.com"  }
+    , Receiver { name = "Leo",    email = "leo@dicaprio.com"  }
+    , Receiver { name = "Tom",    email = "tom@m.com"         }
+    , Receiver { name = "Simon",  email = "simon@example.com" }
+    , Receiver { name = "Boris",  email = "contact@boris.com" }
+    , Receiver { name = "Donald", email = "donald@duck.com"   }
+    ]
+
+-- | Reads messages from the channel and delivers them,
+-- as long as the channel is not empy.
+postman :: String -> TBMChan Card -> IO ()
+postman name chan
+  = keepDelivering
+  where
+    keepDelivering = do
+      empty <- atomically (isEmptyTBMChan chan)
+      unless empty $ do
+        deliverCard
+        threadDelay 1000000 -- 1 second. TODO make this random
+        keepDelivering
+
+    deliverCard = do
+      card <- atomically $ do
+        card <- readTBMChan chan
+        pure card
+
+      putStrLn $
+           "Card delivered by "
+        <> name
+        <> " "
+        <> (show card)
+
+-- | Makes cards and puts them on the given channel.
+sendCards :: TBMChan Card -> IO ()
+sendCards chan
+  = do
+      let cards = mkCard <$> receivers
+      -- write all cards to the channel
+      _ <- atomically $ forM_ cards $ writeTBMChan chan
+      pure ()
+  where
+    mkCard r =
+      Card
+        { from    = "hello@yourbestfriend.co.uk"
+        , to      = email r
+        , subject = "hohoho!"
+        , message = "Hello " <> name r <> ", Have a merry little christmas!"
+        }
 
 main :: IO ()
 main = do
-  putStrLn "hello world"
+  -- | create a bounded channel with max bound 10
+  chan <- atomically (newTBMChan 10)
+
+  sendCards chan
+  postman "Pat" chan
+  postman "Bob" chan
+
+  -- TODO: use forkIO to add concurrency
+
+  putStrLn "Finished!"
